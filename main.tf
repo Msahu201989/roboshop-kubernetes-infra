3be0fe45b0f483d9ce554bec9d9cbe535d7d9de4
Mukesh <msahu201989@gmail.com>
2022-12-24 11:18:37 +0530

    v

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ module "EKS" {
   MIN_SIZE                = 1
   CREATE_ALB_INGRESS      = false
   CREATE_EXTERNAL_SECRETS = true
-  INSTALL_KUBE_METRICS    = true
+  INSTALL_KUBE_METRICS    = false
   CREATE_SCP              = false
 #  CREATE_NGINX_INGRESS    = true
 }
